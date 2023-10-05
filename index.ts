1443cc232d2dd62ebddf0b8c79be0b347f5fbba8
tohrxyz <tomas.hrib@proton.me>
2023-10-05 18:14:27 +0200

    feat: get proposals using multisigclient + respond on !proposals

--- index.ts
+++ index.ts
@@ -3,9 +3,34 @@ import {
     SimpleFsStorageProvider,
     AutojoinRoomsMixin
 } from "matrix-bot-sdk";
-import 'dotenv/config'
+import 'dotenv/config';
+
+import {
+  MultisigClient,
+  MultisigProposalListItem,
+  ProposalQueryParams,
+  ProposalSortBy,
+} from "@aragon/sdk-client";
+import { ProposalStatus, SortDirection } from "@aragon/sdk-client-common";
+import { context } from "./contextStore";
 
 const main = async () => {
+  const daoAddress = process.env.DAO_ADDRESS;
+  if (!daoAddress) throw new Error("DAO_ADDRESS must be set");
+  console.log("daoAddress: ", daoAddress)
+  const multisigClient = new MultisigClient(context);
+
+  const queryParams: ProposalQueryParams = {
+    direction: SortDirection.ASC, // optional. otherwise, DESC
+    sortBy: ProposalSortBy.CREATED_AT, //optional. otherwise, NAME, VOTES (POPULARITY coming soon)
+    status: ProposalStatus.ACTIVE, // optional. otherwise, PENDING, SUCCEEDED, EXECUTED, DEFEATED
+    daoAddressOrEns: daoAddress, // or my-dao.dao.eth
+  };
+
+  const multisigProposals: MultisigProposalListItem[] = await multisigClient
+  .methods.getProposals(queryParams);
+  console.log(multisigProposals);
+
   const homeserverUrl = process.env.HOMESERVER_URL;
   if (!homeserverUrl) throw new Error("HOMESERVER_URL must be set");
   
@@ -29,6 +54,13 @@ const main = async () => {
                   "msgtype": "m.notice",
                   "body": `${sender} pong!`
               });
+          }
+
+          if (body === "!proposals") {
+            client.sendMessage(roomId, {
+                "msgtype": "m.notice",
+                "body": `${JSON.stringify(multisigProposals)}`
+            });
           }
       }
   });
