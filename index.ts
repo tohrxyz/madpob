8e6688fb90c35dc087a682170aea4604c9ce43f2
tohrxyz <tomas.hrib@proton.me>
2023-10-05 17:44:05 +0200

    feat: bot responds '@sender pon' when message '!ping'

--- index.ts
+++ index.ts
@@ -3,18 +3,35 @@ import {
     SimpleFsStorageProvider,
     AutojoinRoomsMixin
 } from "matrix-bot-sdk";
-
 import 'dotenv/config'
 
-const homeserverUrl = process.env.HOMESERVER_URL;
-if (!homeserverUrl) throw new Error("HOMESERVER_URL must be set");
+const main = async () => {
+  const homeserverUrl = process.env.HOMESERVER_URL;
+  if (!homeserverUrl) throw new Error("HOMESERVER_URL must be set");
+  
+  const accessToken = process.env.ACCESS_TOKEN;
+  if (!accessToken) throw new Error("ACCESS_TOKEN must be set");
+  
+  const storage = new SimpleFsStorageProvider("bot.json");
+  
+  const client = new MatrixClient(homeserverUrl, accessToken, storage);
+  AutojoinRoomsMixin.setupOnClient(client);
+  
+  client.start().then(() => console.log("Client started!"));
+  
+  client.on("room.message", (roomId, event) => {
+      if (event["content"]["msgtype"] === "m.text") {
+          const sender = event["sender"];
+          const body = event["content"]["body"];
+  
+          if (body === "!ping") {
+              client.sendMessage(roomId, {
+                  "msgtype": "m.notice",
+                  "body": `${sender} pong!`
+              });
+          }
+      }
+  });
+}
 
-const accessToken = process.env.ACCESS_TOKEN;
-if (!accessToken) throw new Error("ACCESS_TOKEN must be set");
-
-const storage = new SimpleFsStorageProvider("bot.json");
-
-const client = new MatrixClient(homeserverUrl, accessToken, storage);
-AutojoinRoomsMixin.setupOnClient(client);
-
-client.start().then(() => console.log("Client started!"));
+main();
