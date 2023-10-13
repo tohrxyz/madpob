4aa4410b7ecb1ca9ee894d8b80b1c5f8b3da5b29
tohrxyz <tomas.hrib@proton.me>
2023-10-13 20:53:47 +0200

    refactor: clean up index source

--- index.ts
+++ index.ts
@@ -3,76 +3,21 @@ import {
     SimpleFsStorageProvider,
     AutojoinRoomsMixin,
 } from "matrix-bot-sdk";
-import 'dotenv/config';
 
-import {
-  Client, ProposalQueryParams, ProposalSortBy, TokenVotingClient, TokenVotingProposalListItem,
-} from "@aragon/sdk-client";
-import { context } from "./contextStore";
-import { SortDirection, ProposalStatus } from "@aragon/sdk-client-common";
+import { 
+  getDaoDetails,
+  getNewestProposal,
+  periodicProposalScan
+} from "./helpers";
 
-const isProposalNew = (startDate: number) => {
-  const now = Date.now() / 1000;
-  const startDateSeconds = startDate / 1000;
-
-  const diff = now - startDateSeconds;
-
-  console.log("now: ", now)
-  console.log("startDate: ", startDateSeconds)
-  console.log("diff in isPropnew: ", diff);
-
-  if (diff < 600 && diff > 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-const getDaoDetails = async () => {
-  const aragonClient = new Client(context);
-  const daoAddress = process.env.DAO_ADDRESS;
-  
-  if (!daoAddress) throw new Error("DAO_ADDRESS must be set");
-
-  const daoDetails = await aragonClient.methods.getDao(daoAddress).then((dao) => {
-    return dao;
-  });
-  return daoDetails;
-}
-
-const getNewestProposal = async () => {
-  const daoAddress = process.env.DAO_ADDRESS;
-  if (!daoAddress) throw new Error("DAO_ADDRESS must be set");
-
-  const tokenVotingClient: TokenVotingClient = new TokenVotingClient(context);
-
-  const queryParams: ProposalQueryParams = {
-    skip: 0, // optional
-    limit: 10, // optional
-    direction: SortDirection.DESC, // optional, otherwise DESC ("descending")
-    sortBy: ProposalSortBy.CREATED_AT, // optional, otherwise NAME, VOTES (POPULARITY coming soon)
-    // status: ProposalStatus.ACTIVE, // optional, otherwise PENDING, SUCCEEDED, EXECUTED, DEFEATED
-    daoAddressOrEns: daoAddress
-  };
-
-  const tokenVotingProposals: TokenVotingProposalListItem[] = await tokenVotingClient.methods.getProposals(queryParams);
-  // console.log("tokenVotingProposals: ", tokenVotingProposals)
-
-  const newestProposal = tokenVotingProposals[0];
-  return newestProposal;
-}
+import { 
+  daoAddress,
+  homeserverUrl,
+  accessToken,
+  roomID
+ } from "./envs/constants";
 
 const main = async () => {
-  const daoAddress = process.env.DAO_ADDRESS;
-  if (!daoAddress) throw new Error("DAO_ADDRESS must be set");
-  console.log("daoAddress: ", daoAddress)
-  
-  const homeserverUrl = process.env.HOMESERVER_URL;
-  if (!homeserverUrl) throw new Error("HOMESERVER_URL must be set");
-  
-  const accessToken = process.env.ACCESS_TOKEN;
-  if (!accessToken) throw new Error("ACCESS_TOKEN must be set");
-  
   const storage = new SimpleFsStorageProvider("bot.json");
   
   const client = new MatrixClient(homeserverUrl, accessToken, storage);
@@ -80,53 +25,7 @@ const main = async () => {
   
   client.start().then(() => console.log("Client started!"));
 
-  const roomID = process.env.ROOM_ID;
-  if (!roomID) throw new Error("ROOM_ID must be set");
-
-  // setInterval(async () => {
-  //   client.sendMessage(roomID, {
-  //     "msgtype": "m.notice",
-  //     "body": `Commands: !ping, !dao, !help`
-  //   });
-  // }, 43200000);
-
-  setInterval(async () => {
-    console.log(`${Date.now()}`, "Checking for new proposals");
-    const newestProposal = await getNewestProposal();
-    // console.log("newestProposal: ", newestProposal);
-    
-    if (!newestProposal) {
-      console.log("No newest proposal found");
-      return;
-    }
-
-    const title = newestProposal.metadata.title;
-    const summary = newestProposal.metadata.summary;
-    
-    const timeLeft = (newestProposal.endDate.getTime() / 1000) - (Date.now() / 1000);
-    console.log("timeLeft: ", timeLeft);
-    let timeLeftString = "Ended";
-    if (timeLeft > 0) {
-      const days = Math.floor(timeLeft / (24 * 60 * 60));
-      const hours = Math.floor((timeLeft % (24 * 60 * 60)) / (60 * 60));
-      const minutes = Math.floor((timeLeft % (60 * 60)) / 60);
-      const seconds = Math.floor(timeLeft % 60);
-      timeLeftString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
-    }
-
-    const url = `https://app.aragon.org/#/daos/polygon/${daoAddress}/governance/proposals/${newestProposal.id}`;
-
-    const markdownFormatted = `New proposal was published!!!\n\nTitle: ${title}\n\nSummary: ${summary}\n\nTime left: ${timeLeftString}\n\nLink: ${url}`;
-
-    console.log("newestProposal.startDate: ", newestProposal.startDate.getTime() / 1000)
-    console.log("newestProposal.startDate.getSeconds(): ", newestProposal.startDate.getSeconds())
-    if (isProposalNew(newestProposal.startDate.getTime())) {
-      client.sendMessage(roomID, {
-        "msgtype": "m.text",
-        "body": markdownFormatted
-      })  
-    }
-  }, 600000)
+  periodicProposalScan(client);
   
   client.on("room.message", async (roomId, event) => {
       if (event["content"]["msgtype"] === "m.text") {
